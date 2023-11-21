13bedb5d3fb0f887d3435beb0708f17fa602d70d
alexonthespot7 <bgw595@myy.haaga-helia.fi>
2023-11-21 15:53:31 +0200

    Competitors new design

--- src/Components/UserComponents/Competitors.js
+++ src/Components/UserComponents/Competitors.js
@@ -1,14 +1,7 @@
-import TableContainer from '@mui/material/TableContainer';
-import Table from '@mui/material/Table';
-import TableHead from '@mui/material/TableHead';
-import TableBody from '@mui/material/TableBody';
-import TableRow from '@mui/material/TableRow';
-import TableCell from '@mui/material/TableCell';
+
 import Paper from '@mui/material/Paper';
-import { Box, Typography } from '@mui/material';
-import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-alpine.css';
+import { Box, Card, List, ListItem, ListItemText, ListSubheader, Typography } from '@mui/material';
+
 
 const competitors = [
     {
@@ -41,46 +34,72 @@ const competitors = [
         isOut: true,
         stage: '1/4'
     },
+    {
+        username: 'Maessy',
+        isOut: false,
+        stage: '1/2'
+    },
+    {
+        username: 'Menet',
+        isOut: false,
+        stage: '1/2'
+    },
+    {
+        username: 'Greedy',
+        isOut: true,
+        stage: '1/4'
+    },
+    {
+        username: 'Dizzy',
+        isOut: true,
+        stage: '1/4'
+    },
+    {
+        username: 'Maessy',
+        isOut: false,
+        stage: '1/2'
+    },
+    {
+        username: 'Menet',
+        isOut: false,
+        stage: '1/2'
+    },
+    {
+        username: 'Greedy',
+        isOut: true,
+        stage: '1/4'
+    },
+    {
+        username: 'Dizzy',
+        isOut: true,
+        stage: '1/4'
+    },
 
 ];
 
 export default function Competitors() {
 
-    const columnDefs = [
-        { headerName: 'Username', field: 'username', sortable: true, filter: true },
-        { headerName: 'Stage', field: 'stage', sortable: true, filter: true },
-        {
-            headerName: 'Status',
-            field: 'status',
-            sortable: true,
-            filter: true,
-            cellRenderer: (params) => (params.value ? 'Out' : 'In'),
-        },
-    ];
-
-    const rowData = competitors.map((competitor, index) => ({
-        id: index + 1,
-        username: competitor.username,
-        stage: competitor.stage,
-        status: competitor.isOut,
-    }));
-
-
     return (
-        <Box sx={{ mt: 3 }} width='70%' display='flex' flexDirection='column' justifyContent='center' height='100%'>
-            <Typography variant="h4" sx={{ mb: 4 }}>
+        <Box sx={{ mt: 5 }} width='100%' display='flex' flexDirection='column' justifyContent='center' alignItems='center' height='90%' >
+            <Typography variant="h4" sx={{ mb: 2 }}>
                 Competitors
             </Typography>
-            <div
-                className="ag-theme-alpine"
-                style={{ height: '50%', width: '100%', border: '1px solid #ccc' }}
+            <List
+                subheader={<ListSubheader color='secondary' sx={{ backgroundColor: '#f0f0f0', boxShadow: 7, py: '1%', px: '2%', m: '1%', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                    <Typography align='left' sx={{ flex: 1 }} fontWeight={900} fontSize={20}>Stage</Typography>
+                    <Typography align='center' sx={{ flex: 10 }} fontWeight={900} fontSize={20}>Username</Typography>
+                    <Typography align='right' sx={{ flex: 1 }} fontWeight={900} fontSize={20}>Status</Typography>
+                </ListSubheader>}
+                sx={{ width: '70%', maxHeight: '65%', overflow: 'auto' }}
             >
-                <AgGridReact
-                    columnDefs={columnDefs}
-                    rowData={rowData}
-                    defaultColDef={{ flex: 1, minWidth: 150 }}
-                />
-            </div>
+                {competitors.map((competitor, index) => (
+                    <Card key={index} sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', py: '2%', px: '3%', m: '1%' }}>
+                        <Typography align='left' sx={{ flex: 1 }}>{competitor.stage} </Typography>
+                        <Typography align='center' sx={{ flex: 10 }}>{competitor.username} </Typography>
+                        <Typography align='right' sx={{ flex: 1 }}>{competitor.isOut ? 'Out' : 'In'} </Typography>
+                    </Card>
+                ))}
+            </List>
         </Box >
     );
 }
