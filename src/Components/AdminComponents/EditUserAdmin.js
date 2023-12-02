62b15fa7173f2877259e0c6398e1a814fd82f51f
alexonthespot7 <bgw595@myy.haaga-helia.fi>
2023-12-02 02:43:57 +0200

    Edit user functionality for admin is connected to back-end

--- src/Components/AdminComponents/EditUserAdmin.js
+++ src/Components/AdminComponents/EditUserAdmin.js
@@ -1,10 +1,17 @@
 import EditIcon from '@mui/icons-material/Edit';
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, MenuItem, TextField } from '@mui/material';
+import { Box, Button, CircularProgress, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, MenuItem, TextField } from '@mui/material';
+import { useContext } from 'react';
 import { useState } from 'react';
+import ContextWrapper from '../../context/ContextWrapper';
+import Cookies from 'js-cookie';
 
-export default function EditUserAdmin({ user, bracketMade }) {
+export default function EditUserAdmin({ fetchUsersPageForm, user, bracketMade }) {
+    const [loading, setLoading] = useState(false);
     const [openDialog, setOpenDialog] = useState(false);
     const [userUpdated, setUserUpdated] = useState({});
+
+    const { makeErrorAlert, makeSuccessAlert, makeBlackAlert, isNormalSize, size, deleteCookies } = useContext(ContextWrapper);
+
     const handleClickOpen = () => {
         setUserUpdated(user);
         setOpenDialog(true);
@@ -14,98 +21,190 @@ export default function EditUserAdmin({ user, bracketMade }) {
         setOpenDialog(false);
     }
 
-    const inputChanged = () => { }
+    const inputChanged = (event) => {
+        setUserUpdated({ ...userUpdated, [event.target.name]: event.target.value });
+    }
 
-    const handleSave = () => { }
+    const handleSave = () => {
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        for (const key of Object.keys(userUpdated)) {
+            if (userUpdated[key] === '') {
+                makeBlackAlert(`${key} is mandatory`);
+                return null;
+            }
+        }
+        if (userUpdated.username.includes(' ')) {
+            makeBlackAlert('Username must not include whitespaces');
+        } else if (userUpdated.username.length > 15) {
+            makeBlackAlert('The username must not be longer than 15 symbols');
+        } else if (!emailPattern.test(userUpdated.email)) {
+            makeBlackAlert('Please provide valid email');
+        } else {
+            setLoading(true);
+            editUser();
+        }
+    }
+
+    const editUser = async () => {
+        try {
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/admin/updateuser/${user.id}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': Cookies.get('jwt')
+                },
+                body: JSON.stringify(userUpdated)
+            });
+            if (response.ok) {
+                fetchUsersPageForm();
+                setOpenDialog(false);
+                setLoading(false);
+                makeSuccessAlert('User was updated successfully');
+            } else if (response.status === 401) {
+                makeErrorAlert(await response.text());
+                deleteCookies();
+            } else if (response.status === 403) {
+                makeErrorAlert(await response.text());
+                //redirect to main page;
+            } else {
+                setLoading(false);
+                makeErrorAlert(await response.text());
+            }
+        } catch (error) {
+            makeErrorAlert(error.message);
+        }
+    }
 
     return (
         <>
-            <IconButton onClick={handleClickOpen}>
-                <EditIcon color='secondary' />
+            <IconButton
+                size={size}
+                onClick={handleClickOpen}
+            >
+                <EditIcon fontSize={size} color='secondary' />
             </IconButton>
             <Dialog open={openDialog} onClose={handleClose}>
-                <DialogTitle>Edit User {user.id}</DialogTitle>
-                <DialogContent>
-                    <TextField
-                        margin="dense"
-                        name='username'
-                        value={userUpdated.username}
-                        onChange={inputChanged}
-                        label='Username'
-                        fullWidth
-                        variant="standard"
-                    />
-                    <TextField
-                        margin="dense"
-                        name='email'
-                        value={userUpdated.email}
-                        onChange={inputChanged}
-                        label='Email'
-                        fullWidth
-                        variant="standard"
-                    />
-                    {user.role !== 'ADMIN' &&
-                        <TextField
-                            select
-                            margin="dense"
-                            name='role'
-                            value={userUpdated.role}
-                            onChange={inputChanged}
-                            label='Role'
-                            fullWidth
-                            variant="standard"
-                        >
-                            <MenuItem value={'ADMIN'}>
-                                ADMIN
-                            </MenuItem>
-                            <MenuItem value={'USER'}>
-                                USER
-                            </MenuItem>
-                        </TextField>
-                    }
-                    {!(user.isCompetitor && bracketMade) &&
-                        <TextField
-                            select
-                            margin="dense"
-                            name='isCompetitor'
-                            value={userUpdated.isCompetitor}
-                            onChange={inputChanged}
-                            label='Participant?'
-                            fullWidth
-                            variant="standard"
-                        >
-                            <MenuItem value={true}>
-                                Yes
-                            </MenuItem>
-                            <MenuItem value={false}>
-                                No
-                            </MenuItem>
-                        </TextField>
-                    }
-                    {!user.accountVerified &&
-                        <TextField
-                            select
-                            margin="dense"
-                            name='accountVerified'
-                            value={userUpdated.accountVerified}
-                            onChange={inputChanged}
-                            label='Verified?'
-                            fullWidth
-                            variant="standard"
-                        >
-                            <MenuItem value={true}>
-                                Yes
-                            </MenuItem>
-                            <MenuItem value={false}>
-                                No
-                            </MenuItem>
-                        </TextField>
-                    }
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>Cancel</Button>
-                    <Button onClick={handleSave}>Save</Button>
-                </DialogActions>
+                <DialogTitle fontSize={isNormalSize ? 20 : 17}>Edit User {user.id}</DialogTitle>
+                {!loading &&
+                    <>
+                        <DialogContent>
+                            <TextField
+                                color='secondary'
+                                size={size}
+                                margin="dense"
+                                name='username'
+                                value={userUpdated.username}
+                                onChange={inputChanged}
+                                label='Username'
+                                fullWidth
+                                variant='standard'
+                            />
+                            <TextField
+                                color='secondary'
+                                size={size}
+                                margin="dense"
+                                name='email'
+                                value={userUpdated.email}
+                                onChange={inputChanged}
+                                label='Email'
+                                fullWidth
+                                variant='standard'
+                            />
+                            {user.role !== 'ADMIN' &&
+                                <TextField
+                                    color='secondary'
+                                    size={size}
+                                    select
+                                    margin="dense"
+                                    name='role'
+                                    value={userUpdated.role}
+                                    onChange={inputChanged}
+                                    label='Role'
+                                    fullWidth
+                                    variant='standard'
+                                >
+                                    <MenuItem value={'ADMIN'}>
+                                        ADMIN
+                                    </MenuItem>
+                                    <MenuItem value={'USER'}>
+                                        USER
+                                    </MenuItem>
+                                </TextField>
+                            }
+                            {!bracketMade &&
+                                <TextField
+                                    color='secondary'
+                                    size={size}
+                                    select
+                                    margin="dense"
+                                    name='isCompetitor'
+                                    value={userUpdated.isCompetitor}
+                                    onChange={inputChanged}
+                                    label='Participant?'
+                                    fullWidth
+                                    variant='standard'
+                                >
+                                    <MenuItem value={true}>
+                                        Yes
+                                    </MenuItem>
+                                    <MenuItem value={false}>
+                                        No
+                                    </MenuItem>
+                                </TextField>
+                            }
+                            {!user.accountVerified &&
+                                <TextField
+                                    color='secondary'
+                                    size={size}
+                                    select
+                                    margin="dense"
+                                    name='accountVerified'
+                                    value={userUpdated.accountVerified}
+                                    onChange={inputChanged}
+                                    label='Verified?'
+                                    fullWidth
+                                    variant='standard'
+                                >
+                                    <MenuItem value={true}>
+                                        Yes
+                                    </MenuItem>
+                                    <MenuItem value={false}>
+                                        No
+                                    </MenuItem>
+                                </TextField>
+                            }
+                        </DialogContent>
+                        <DialogActions>
+                            <Button
+                                color='secondary'
+                                size={size}
+                                onClick={handleClose}
+                            >
+                                Cancel
+                            </Button>
+                            <Button
+                                variant='contained'
+                                color='secondary'
+                                size={size}
+                                onClick={handleSave}
+                            >
+                                Save
+                            </Button>
+                        </DialogActions>
+                    </>
+                }
+                {loading &&
+                    <Box
+                        height='100%'
+                        width='100%'
+                        display='flex'
+                        alignItems='center'
+                        justifyContent='center'
+                    >
+                        <CircularProgress color='secondary' />
+                    </Box>
+                }
             </Dialog>
         </>
     );
