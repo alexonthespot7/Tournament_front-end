5751d52aba7e41c2bcda6dece51daca03bc7204d
alexonthespot7 <bgw595@myy.haaga-helia.fi>
2023-12-02 02:44:34 +0200

    Rounds for admin component is now connected to back-end and is responsive

--- src/Components/AdminComponents/RoundsAdmin.js
+++ src/Components/AdminComponents/RoundsAdmin.js
@@ -1,181 +1,66 @@
+import { useContext, useEffect, useState } from 'react';
+import { Box, Button, Card, CircularProgress, IconButton, List, ListItem, ListItemText, ListSubheader, Typography } from '@mui/material';
 import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-material.css';
-import NorthIcon from '@mui/icons-material/North';
-import SouthIcon from '@mui/icons-material/South';
-import { Box, Button, Card, IconButton, List, ListItem, ListItemText, ListSubheader, Typography } from '@mui/material';
+import 'ag-grid-community/styles/ag-theme-quartz.css';
 import { AgGridReact } from 'ag-grid-react';
-import { useState } from 'react';
 import SetResult from './SetResult';
-
-
-const roundsFormAdmin = {
-    "allRounds": [
-        {
-            "roundid": 1,
-            "result": "No",
-            "user1": {
-                "id": 3,
-                "username": "wanyeser",
-                "passwordHash": "$2a$12$0Mu/91y.kvDE7rj0ZXrWkOxUISfqEuQcXyU.luDJIe7DW2W/eqUYq",
-                "role": "USER",
-                "isOut": false,
-                "isCompetitor": true,
-                "email": "mrbudach2@mail.ru",
-                "verificationCode": null,
-                "accountVerified": true,
-                "stage": {
-                    "stageid": 2,
-                    "stage": "1/2",
-                    "isCurrent": true
-                }
-            },
-            "user2": {
-                "id": 2,
-                "username": "danrey",
-                "passwordHash": "$2a$12$0Mu/91y.kvDE7rj0ZXrWkOxUISfqEuQcXyU.luDJIe7DW2W/eqUYq",
-                "role": "USER",
-                "isOut": false,
-                "isCompetitor": true,
-                "email": "bgw2595@myy.haaga-helia.fi",
-                "verificationCode": null,
-                "accountVerified": true,
-                "stage": {
-                    "stageid": 2,
-                    "stage": "1/2",
-                    "isCurrent": true
-                }
-            },
-            "stage": {
-                "stageid": 2,
-                "stage": "1/2",
-                "isCurrent": true
-            }
-        },
-        {
-            "roundid": 2,
-            "result": "loginTest autowin",
-            "user1": {
-                "id": 4,
-                "username": "loginTest",
-                "passwordHash": "$2a$12$19lxeD0nHwNrMxnGhWFNoOLMC/xOxd81ug1D.fboYQeoRHjyR9hym",
-                "role": "USER",
-                "isOut": false,
-                "isCompetitor": true,
-                "email": "login.test@gmail.com",
-                "verificationCode": null,
-                "accountVerified": true,
-                "stage": {
-                    "stageid": 2,
-                    "stage": "1/2",
-                    "isCurrent": true
-                }
-            },
-            "user2": null,
-            "stage": {
-                "stageid": 2,
-                "stage": "1/2",
-                "isCurrent": true
-            }
-        },
-        {
-            "roundid": 3,
-            "result": "No",
-            "user1": null,
-            "user2": null,
-            "stage": {
-                "stageid": 4,
-                "stage": "final",
-                "isCurrent": false
-            }
-        },
-        {
-            "roundid": 1,
-            "result": "No",
-            "user1": {
-                "id": 3,
-                "username": "wanyeser",
-                "passwordHash": "$2a$12$0Mu/91y.kvDE7rj0ZXrWkOxUISfqEuQcXyU.luDJIe7DW2W/eqUYq",
-                "role": "USER",
-                "isOut": false,
-                "isCompetitor": true,
-                "email": "mrbudach2@mail.ru",
-                "verificationCode": null,
-                "accountVerified": true,
-                "stage": {
-                    "stageid": 2,
-                    "stage": "1/2",
-                    "isCurrent": true
-                }
-            },
-            "user2": {
-                "id": 2,
-                "username": "danrey",
-                "passwordHash": "$2a$12$0Mu/91y.kvDE7rj0ZXrWkOxUISfqEuQcXyU.luDJIe7DW2W/eqUYq",
-                "role": "USER",
-                "isOut": false,
-                "isCompetitor": true,
-                "email": "bgw2595@myy.haaga-helia.fi",
-                "verificationCode": null,
-                "accountVerified": true,
-                "stage": {
-                    "stageid": 2,
-                    "stage": "1/2",
-                    "isCurrent": true
-                }
-            },
-            "stage": {
-                "stageid": 2,
-                "stage": "1/2",
-                "isCurrent": true
-            }
-        },
-        {
-            "roundid": 2,
-            "result": "loginTest autowin",
-            "user1": {
-                "id": 4,
-                "username": "loginTest",
-                "passwordHash": "$2a$12$19lxeD0nHwNrMxnGhWFNoOLMC/xOxd81ug1D.fboYQeoRHjyR9hym",
-                "role": "USER",
-                "isOut": false,
-                "isCompetitor": true,
-                "email": "login.test@gmail.com",
-                "verificationCode": null,
-                "accountVerified": true,
-                "stage": {
-                    "stageid": 2,
-                    "stage": "1/2",
-                    "isCurrent": true
-                }
-            },
-            "user2": null,
-            "stage": {
-                "stageid": 2,
-                "stage": "1/2",
-                "isCurrent": true
-            }
-        },
-        {
-            "roundid": 3,
-            "result": "No",
-            "user1": null,
-            "user2": null,
-            "stage": {
-                "stageid": 4,
-                "stage": "final",
-                "isCurrent": false
-            }
-        }
-    ],
-    "doesWinnerExist": false,
-    "currentStageFinished": true
-}
+import Cookies from 'js-cookie';
+import ContextWrapper from '../../context/ContextWrapper';
+import ConfirmResults from './ConfirmResults';
 
 export const findUsernameOfContestant = (username) => {
     return username ? username : '—';
 }
 
 export default function RoundsAdmin() {
+    const [loading, setLoading] = useState(true);
+    const [roundsFormAdmin, setRoundsFormAdmin] = useState(null);
+
+    const { windowSize, makeErrorAlert, makeWarningAlert, headerVariant, isNormalSize, size, deleteCookies } = useContext(ContextWrapper);
+
+    useEffect(() => {
+        if (Cookies.get('role') === 'ADMIN') {
+            fetchRoundsForm();
+        } else {
+            //redirect to main page
+            makeWarningAlert('You don\'t have permission to access this page');
+        }
+    }, []);
+
+    const fetchRoundsForm = async () => {
+        try {
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/admin/rounds`, {
+                method: 'GET',
+                headers: { 'Authorization': Cookies.get('jwt') }
+            });
+            if (!response.ok) {
+                if (response.status === 401) {
+                    deleteCookies();
+                    makeErrorAlert('Your session has expired, login again please');
+                } else if (response.status === 403) {
+                    makeErrorAlert(await response.text());
+                    //redirect to main page
+                } else {
+                    makeErrorAlert(await response.text());
+                }
+                return null;
+            }
+            if (response.status === 202) {
+                makeErrorAlert('The bracket was not made yet');
+                //redirect to main page
+                return null;
+            }
+
+            response.json()
+                .then(data => {
+                    setRoundsFormAdmin(data);
+                    setLoading(false);
+                })
+                .catch(err => makeErrorAlert(err.message));
+        } catch (error) {
+            makeErrorAlert(error.message);
+        }
+    }
 
     const columnDefs = [
         { headerName: 'Stage', field: 'stage.stage' },
@@ -192,37 +77,68 @@ export default function RoundsAdmin() {
                 const round = param.data;
                 if (!roundsFormAdmin.doesWinnerExist && stageCurrent && !isAutoWin) {
                     return (
-                        <SetResult round={round} />
+                        <SetResult
+                            fetchRoundsForm={fetchRoundsForm}
+                            round={round}
+                            setLoading={setLoading}
+                        />
                     );
                 }
                 return null; // Hide the cell if conditions are not met
             },
-            width: 100,
         },
     ];
 
     return (
-        <Box sx={{ mt: 10 }} width='100%' display='flex' flexDirection='column' justifyContent='center' alignItems='center' height='90%' >
-            <Typography variant="h4" sx={{ mb: 2 }}>
-                Rounds
-            </Typography>
-            <div className="ag-theme-material" style={{ width: '70%', height: '65%', marginBottom: '1.5%' }}>
-                <AgGridReact
-                    rowData={roundsFormAdmin.allRounds}
-                    columnDefs={columnDefs}
-                    defaultColDef={{ flex: 1, resizable: true, filter: true, sortable: true, cellStyle: { 'textAlign': 'left' } }}
-                    pagination={true}
-                    paginationPageSize={7}
-                    animateRows={true}
-                />
-            </div>
-            {roundsFormAdmin.currentStageFinished &&
-                <Box display='flex' width='70%' justifyContent='left'>
-                    <Button variant='contained' color='secondary'>
-                        Confirm results
-                    </Button>
-                </Box>
+        <Box
+            mt='5%'
+            width='100%'
+            display='flex'
+            flexDirection='column'
+            justifyContent='center'
+            alignItems='center'
+            height='90%'
+        >
+            {!loading &&
+                <>
+                    <Typography variant={headerVariant} sx={{ mb: '1.5%' }}>
+                        Rounds
+                    </Typography>
+                    <div
+                        className='ag-theme-quartz'
+                        style={{
+                            width: isNormalSize ? '70%' : '85%',
+                            height: '65%',
+                            marginBottom: '1.5%'
+                        }}
+                    >
+                        <AgGridReact
+                            rowData={roundsFormAdmin.allRounds}
+                            columnDefs={columnDefs}
+                            defaultColDef={{
+                                resizable: true,
+                                filter: true,
+                                sortable: true,
+                                cellStyle: { 'textAlign': 'left' }
+                            }}
+                            animateRows={true}
+                        />
+                    </div>
+                    {roundsFormAdmin.currentStageFinished &&
+                        <Box
+                            display='flex'
+                            width={isNormalSize ? '70%' : '85%'}
+                            justifyContent='left'
+                        >
+                            <ConfirmResults
+                                setLoading={setLoading}
+                                fetchRoundsForm={fetchRoundsForm}
+                            />
+                        </Box>
+                    }
+                </>
             }
+            {loading && <CircularProgress color='secondary' />}
         </Box>
     );
 }
